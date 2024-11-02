02fa07a575066bc6ea8072cc512fb9ba49780642
Sputnik-v <igortronin2705@gmail.com>
2024-11-02 17:35:51 +0400

    finish 5

--- 4_practice/index.php
+++ 4_practice/index.php
@@ -1,1 +1,102 @@
 <?php
+/////////////////Циклы////////////////
+
+//1
+$i = 0;
+
+while($i <= 100){
+    echo $i . "\n";
+    $i++;
+}
+
+//2
+
+$i = 100;
+
+while($i >= 0){
+    echo $i . "\n";
+    $i--;
+}
+
+//3
+for ($i = 1; $i <= 100; $i++) {
+    if($i % 2 == 0){
+        echo $i . "\n";
+    }
+}
+
+//4
+$i = 0;
+$arr = [];
+while($i <= 10){
+    $arr[$i] = 'x';
+    $i++;
+}
+
+//5
+$i = 0;
+$arr = [];
+while($i <= 10){
+    $arr[$i] = $i;
+    $i++;
+}
+
+//6
+$arr = [1, 3, 4, 6, 3, 6, 10];
+
+foreach ($arr as $elem){
+    if($elem > 0 && $elem < 10){
+        echo $elem;
+    }
+}
+
+//7
+$arr = [1, 3, 4, 5, 6, 3, 6, 10];
+
+foreach ($arr as $elem){
+    if($elem === 5){
+        echo "Have 5";
+    }
+}
+
+//8
+$arr = [1, 3, 4, 5, 6, 3, 6, 10];
+$sum = 0;
+foreach ($arr as $elem){
+    if(is_numeric($elem)){
+        $sum += $elem;
+    }else{
+        echo "not int";
+        return;
+    }
+}
+echo $sum;
+
+//9
+$arr = [1, 3, 3];
+$sum = 0;
+foreach ($arr as $elem){
+    if(is_numeric($elem)){
+        $sum += $elem * $elem;
+    }else{
+        echo "not int";
+        return;
+    }
+}
+echo $sum;
+
+//10
+$arr = [5,5,5,5];
+$sum = 0;
+foreach ($arr as $elem){
+    $sum += $elem;
+}
+$average = $sum / count($arr);
+echo $average;
+
+//11
+$n = 5;
+for ($i = 1; $i <= $n; $i++) {
+    $factorial = $i * $n;
+}
+echo $factorial;
--- 5_practice/index.php
+++ 5_practice/index.php
@@ -1,1 +1,29 @@
 <?php
+//////////////////Functions///////////////////
+
+//1
+function getDivisors($n) {
+    $arr = [];
+    for ($i = 1; $i <= $n; $i++) {
+        if ($n % $i == 0) {
+            $arr [] = $i;
+        }
+    }
+    return $arr;
+}
+
+//2
+function getCommonDivisors($num1, $num2) {
+    return array_intersect(getDivisors($num1), getDivisors($num2));
+}
+
+//3
+function getSum($number){
+    return array_sum(str_split($number));
+}
+
+//4
+function getStringDay($dow_numeric){
+    $dow_text = date('D', strtotime("Sunday +{$dow_numeric} days"));
+    return $dow_text;
+}
